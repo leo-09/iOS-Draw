64fb20f1d5497933ccab12549397a23ffd75c409
liyy <1290374862@qq.com>
2016-11-28 16:45:48 +0800

    iOS绘图技术－UIBezierPath

---
+++ BezierPath/BezierPath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BezierPath
+//
+//  Created by liyy on 16/11/28.
+//  Copyright © 2016年 liyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
